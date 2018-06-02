c65b99f61add26356071d303aedaba5b8f85e8ac
Rajat Jindal <rajatjindal83@gmail.com>
2018-06-02 06:43:16 -0700

    use latest versions

--- WordPress/deploy.sh
+++ WordPress/deploy.sh
@@ -1,11 +1,11 @@
 # Create local volumes for MySQL
-kubectl create -f local-volumes.yaml
+kubectl apply -f local-volumes.yaml
 
 # Create secret for WordPress
 kubectl create secret generic mysql-pass --from-file=password.txt
 
 # Create MySQL deployment
-kubectl create -f ./mysql-deployment.yaml
+kubectl apply -f ./mysql-deployment.yaml
 
 # Create WordPress deployment
-kubectl create -f ./wordpress-deployment.yaml
+kubectl apply -f ./wordpress-deployment.yaml
